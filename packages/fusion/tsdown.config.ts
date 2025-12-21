d64407bc0c2a2659c1d6fdc7e28eeeaa59d90ead
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2025-12-21 15:20:59 +0800

    refactor: update tsdown config

--- packages/fusion/tsdown.config.ts
+++ packages/fusion/tsdown.config.ts
@@ -1,41 +1,26 @@
-import type { UserConfig } from "tsdown";
+import { defineConfig } from "@apst/tsdown";
+import { cjsPreset, dtsPreset, esmPreset } from "@apst/tsdown/presets";
 
-import { defineConfig } from "tsdown";
-
-const options: UserConfig = {
-    entry: {
-        // public
-        index: "./src/index.ts",
-        chunk: "./src/chunk.ts",
-        // internal
-        "@types/base/split": "./src/@types/base/split.ts",
-        "@types/base/check": "./src/@types/base/check.ts",
-        "@types/base/merge": "./src/@types/base/merge.ts",
-    },
-    dts: false,
-    outDir: "./dist",
-    clean: true,
-    platform: "browser",
-    treeshake: true,
-    sourcemap: true,
-    minify: false,
-    shims: true,
-    unbundle: true,
-    inputOptions: {
-        experimental: {
-            attachDebugInfo: "none",
+export default defineConfig(
+    {
+        entry: {
+            // public
+            index: "./src/index.ts",
+            chunk: "./src/chunk.ts",
+            // internal
+            "@types/base/split": "./src/@types/base/split.ts",
+            "@types/base/check": "./src/@types/base/check.ts",
+            "@types/base/merge": "./src/@types/base/merge.ts",
         },
+        platform: "browser",
     },
-};
-
-export default defineConfig([
-    {
-        ...options,
-        format: "esm",
-    },
-    {
-        ...options,
-        dts: true,
-        format: "cjs",
-    },
-]);
+    [
+        esmPreset(),
+        cjsPreset(),
+        dtsPreset({
+            presetOptions: {
+                performanceMode: true,
+            },
+        }),
+    ],
+);
